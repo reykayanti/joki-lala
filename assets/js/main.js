4bbfc1e91c65899a9c5205800b9b79d1cc277cac
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2021-11-26 13:31:14 +0700

    create

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+// link terpilih
+$('a[href*="#"]')
+  // hapus link yang tidak terarah
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              return false;
+            } else {
+              $target.attr("tabindex", "-1");
+              $target.focus();
+            }
+          }
+        );
+      }
+    }
+  });
+$(document).ready(function () {
+  $(".menu-mobile").click(function () {
+    $("nav ul").toggleClass("active");
+    $(this).toggleClass("active");
+  });
+});
